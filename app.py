25dc2f12398ddb61e45a3addb3c9c59d34092ead
Danny <Darnnielnology@gmail.com>
2022-10-18 01:11:41 +0100

    first day commit

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ print("  / |")
 print(" /  |")
 print("/___|")
 
-# Variables
+## Variables
 
-print("The greatest are just a few")
+character_name = "John Doe"
+age = "22"
+occupation = "International Super Spy"
+print(age)
+print("The greatest are just a few " + character_name + ", " + age + ", " + occupation)
