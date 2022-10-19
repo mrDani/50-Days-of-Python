5c2daa48d0dc0285c6afba98773ff09235ba95bb
Danny <Darnnielnology@gmail.com>
2022-10-20 00:36:31 +0100

    third day commit

--- new.py
+++ new.py
@@ -60,3 +60,12 @@ monthConversions = {
 
 print(monthConversions["Feb"])
 print(monthConversions.get("Apr"))
+
+#While Loop
+
+i = 1
+while i <=10:
+    print(i)
+    i += 1
+
+print("Done with loop!")
