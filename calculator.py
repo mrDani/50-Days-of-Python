190472830bf31a3d2f33e45e0682690993209dd5
Danny <Darnnielnology@gmail.com>
2022-10-19 02:15:17 +0100

    second day commit

--- calculator.py
+++ calculator.py
@@ -1,7 +1,24 @@
-num1 = input("Enter a number: ")
-num2 = input("Enter another number: ")
+# num1 = input("Enter first number: ")
+# num2 = input("Enter second number: ")
 
 ## result = int(num1) + int(num2)
-result = float(num1) + float(num2)
+# result = float(num1) + float(num2)
 
-print(result)
+# print(result)
+
+# Calculator 2 
+
+num1 = float(input("Enter first number: "))
+op = input("Enter operator: ")
+num2 = float(input("Enter second number: "))
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*":
+    print(num1 * num2)
+else:
+    print("Invalid Operator")
