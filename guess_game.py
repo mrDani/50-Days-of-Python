d87936a149b2632b5625400a11ceac0b29f8fab2
Danny <Darnnielnology@gmail.com>
2022-10-20 00:53:12 +0100

    third day commit

---
+++ guess_game.py
@@ -0,0 +1,19 @@
+secret_word = "valiente"
+guess = ""
+guess_count = 0 
+guess_limit = 3
+out_of_guesses = False
+
+while guess != secret_word and not(out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("Guess a word: ")
+        if guess != secret_word:
+            print("Wrong guess, Try again")
+        guess_count += 1
+    else:
+        out_of_guesses = True
+
+if out_of_guesses:
+    print("Out of Guesses, You Lose!")
+else:
+    print("You win!")
