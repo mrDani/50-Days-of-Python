b90a8b57c4c80244f604a8b5faf57d67e2772ab8
Danny <Darnnielnology@gmail.com>
2022-10-23 23:41:46 +0100

    Day 6 Commit

--- jsonexample.py
+++ jsonexample.py
@@ -18,3 +18,34 @@ for i in data['emp_details']:
 # Closing file
 f.close()
 
+# Python program to read
+# json file
+
+
+import json
+
+
+# JSON string
+a = '{"name": "Bob", "languages": "English"}'
+
+# deserializes into dict
+# and returns dict.
+y = json.loads(a)
+
+print("JSON string = ", y)
+print()
+
+
+# JSON file
+f = open ('data.json', "r")
+
+# Reading from file
+data = json.loads(f.read())
+
+# Iterating through the json
+# list
+for i in data['emp_details']:
+	print(i)
+
+# Closing file
+f.close()
